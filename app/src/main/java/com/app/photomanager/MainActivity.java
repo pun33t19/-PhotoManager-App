16db693141ec6959dac73f8764a14326b7848044
Puneet Shetty <puneet22088@gmail.com>
2021-08-28 08:41:44 +0530

    Added screenshots

--- app/src/main/java/com/app/photomanager/MainActivity.java
+++ app/src/main/java/com/app/photomanager/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       setTheme(R.style.Theme_PhotoManager);
         setContentView(R.layout.activity_main);
         requestPermissions();
         floatingActionButton=findViewById(R.id.fabimage);
